a1de8d3698db2179873511c1911a0f673530b2a9
k-murahata <kmurahata@gmail.com>
2022-01-17 08:50:52 +0900

    メンタリングにて修正しました

--- ArgsLastPrinter.java
+++ ArgsLastPrinter.java
@@ -1,11 +1,11 @@
 public class ArgsLastPrinter {
-    public static void main(String[] args){
-      int x = args.length;
-      if (x < 1){
-        System.out.println("Nothing");
-    }else{
-        System.out.println(args[x-1]);
-      } 
-    }
+  public static void main(String[] args){
+    int x = args.length;
+    if (x < 1) {
+      System.out.println("なし");
+    } else {
+      System.out.println(args[x - 1]);
+    } 
+  }
 }
 
--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,13 +5,18 @@ public class FizzBuzz{
     
         for (int i = 1;i<=n;i++){
             if (i%3 == 0 && i%5 == 0){
-                System.out.print("FizzBuzz"+" ");
+                System.out.print("FizzBuzz");
             }else if(i%3 == 0){
-                System.out.print("Fizz"+" ");  
+                System.out.print("Fizz");  
             }else if(i%5 == 0){
-                System.out.print("Buzz"+" ");
-            }else
-            System.out.print(i+" ");
+                System.out.print("Buzz");
+            }else{
+            System.out.print(i);
+            }
+            if (i < n){
+            System.out.print(" ");
+            }
+
         }
        
     
