539a463393f524dcff116a8469765f6441af1e80
k-murahata <kmurahata@gmail.com>
2022-02-28 22:38:16 +0900

    課題10-1を修正しました

--- Person.java
+++ Person.java
@@ -2,15 +2,24 @@ package questionOne;
 
 public class Person {
 	String sex;
-    Person(String sex){
+	
+    public Person(String sex) {
     	this.sex = sex;
     }
+    
 	public void speak() {
-		if (this.sex == "man") {
+		if (this.sex.equals("man")) {
 			System.out.println("I'am a man");
 		}
-		if (this.sex == "woman") {
+		if (this.sex.equals("woman")) {
 			System.out.println("I'am a woman");
 		}
+	}
+	
+	public static void main(String[] args) {
+		Person p1 = new Person("man");
+		Person p2 = new Person("woman");
+		p1.speak();
+		p2.speak();
 	}
 }
