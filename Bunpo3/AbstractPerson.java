a11e105e99b0bad485c3a786fae4f6b0a0caa58b
k-murahata <kmurahata@gmail.com>
2022-04-29 18:07:07 +0900

    課題　文法－基本3　2を提出します

---
+++ Bunpo3/AbstractPerson.java
@@ -0,0 +1,9 @@
+package QuestionTwo;
+
+public abstract class AbstractPerson {
+	protected abstract String getGender();
+
+	public void speak() {
+		System.out.println("I'm a " + getGender());
+	}
+}
---
+++ Bunpo3/Woman.java
@@ -0,0 +1,10 @@
+package QuestionTwo;
+
+public class Woman extends AbstractPerson {
+
+	@Override
+	protected String getGender() {
+		return "Woman";
+	}
+
+}
